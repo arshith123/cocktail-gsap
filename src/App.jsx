70e7e3829bd3738268c07a797f529b20c1a00fb5
Arshith <arshith386@gmail.com>
2025-07-09 15:27:00 +0530

    completed about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import CockTail from './components/CockTail';
+import About from './components/About';
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -12,6 +13,7 @@ const App = () => {
             <NavBar />
             <Hero />
             <CockTail />
+            <About/>
         </main>
     )
 }
